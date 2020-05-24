62669cb1cc756e60d633963b7e59d8fc2dfe8c56
RaulDobos <raulpriam@yahoo.com>
2020-05-24 15:42:10 +0300

    implemented CRUD methods in task repository

--- src/main/java/org/fasttrackit/App.java
+++ src/main/java/org/fasttrackit/App.java
@@ -3,10 +3,12 @@ package org.fasttrackit;
 import org.fasttrackit.domain.Task;
 import org.fasttrackit.persistance.TaskRepository;
 import org.fasttrackit.transfer.CreateTaskRequest;
+import org.fasttrackit.transfer.UpdateTaskRequest;
 
 import java.io.IOException;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.List;
 
 public class App 
 {
@@ -17,5 +19,16 @@ public class App
 
         TaskRepository taskRepository = new TaskRepository();
         taskRepository.createTask(request);
+
+//        UpdateTaskRequest updateRequest = new UpdateTaskRequest();
+//        updateRequest.setDone(true);
+//
+//        taskRepository.updateTask(1, updateRequest);
+
+//        taskRepository.deleteTask(1);
+
+        List<Task> tasks = taskRepository.getTasks();
+
+        System.out.println(tasks);
     }
 }
